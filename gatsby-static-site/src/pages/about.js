835b8ecb2987b32ff17b7d16256fb1f1f2ab2b90
Poonammohata <poonammohata2@mail.com>
2020-04-20 16:43:03 +0530

    linked pages using link tag

--- gatsby-static-site/src/pages/about.js
+++ gatsby-static-site/src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
+import {Link} from "gatsby"
 
 const AboutPage=()=>{
     return(
         <div>
             <h1>About Page</h1>
             <p>computer science student,working on the,path to become full stack developer</p>
+            <p>What to contact me, <Link to="/contact">then click here</Link></p>
         </div>
     )
 }
--- gatsby-static-site/src/pages/contact.js
+++ gatsby-static-site/src/pages/contact.js
@@ -5,6 +5,7 @@ const ContactPage=()=>{
         <div>
             <h1>Contact details</h1>
             <p>here are all my social profiles,from which you can contact me</p>
+            <p>Here's my twitter id<a href="https://twitter.com/mohata_poonam" target="_blank">click here</a></p>
         </div>
     )
 }
