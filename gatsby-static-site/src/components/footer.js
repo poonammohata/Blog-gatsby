d5d62d536c890e39f55e216cf56a8c7a30189caa
Poonammohata <poonammohata2@mail.com>
2020-04-23 16:03:57 +0530

    fetched data by using metadata using graphql

--- gatsby-static-site/src/components/footer.js
+++ gatsby-static-site/src/components/footer.js
@@ -1,9 +1,18 @@
 import React from "react"
+import {graphql,useStaticQuery} from "gatsby"
 
 const Footer=()=>{
+    const data=useStaticQuery(graphql`
+    query{
+        site{
+            siteMetaData{
+                author
+            }
+        }
+    }`)
     return(
         <div>
-            <p>created by poonam,@poonam mohata</p>
+            <p>created by {data.site.siteMetaData.author} ,@poonam mohata</p>
         </div>
     )
 }
